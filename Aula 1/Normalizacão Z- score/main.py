789710deb9362ddd4cb60821a062dd68c7f89ca8
RodrigoGalvezLima <rodrigogalvezlima3@gmail.com>
2026-05-28 10:05:37 -0300

    feat: z score normalization implemented

---
+++ Aula 1/Normalizacão Z- score/main.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Leitura da dimensão dos dados
+n, m = map(int, input().split())
+
+# Leitura dos dados X
+X = np.zeros((n, m), dtype=int)
+for i in range(n):
+    X[i] = np.array(list(map(int, input().split())))
+    
+# Leitura da amostra x
+x = np.array(list(map(int, input().split())))
+
+# Calcula média e desvio padrão por coluna
+media = np.mean(X, axis=0)
+desvio = np.std(X, axis=0)  # por padrão usa população, ou seja, divide por n
+
+# Normalização z-score da amostra x
+x = (x - media) / desvio
+
+for x_i in x.flatten():
+    print(int(x_i), end=' ')
